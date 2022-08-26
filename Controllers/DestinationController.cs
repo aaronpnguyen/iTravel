afb86abacfacc1ce11b45e980bbfe7cf100b7295
Amiculay <aaronnguyen1123@gmail.com>
2022-08-26 02:39:06 -0400

    Image validation

--- Controllers/DestinationController.cs
+++ Controllers/DestinationController.cs
@@ -47,19 +47,31 @@ public class DestinationController: Controller
     [HttpPost]
     public IActionResult SubmitDestination(IFormFile file, Destination newDestination)
     {
-        string wwwPath = this.WEBHOST.WebRootPath;
-        string contentPath = this.WEBHOST.ContentRootPath;
+        if (notLogged) return RedirectToAction("LogReg", "User");
+        
+        if (newDestination.City == null || newDestination.State == null || newDestination.Country == null || newDestination.DestinationMessage == null) return DestinationForm();
+        
+        if (file != null)
+        {
+            string wwwPath = this.WEBHOST.WebRootPath;
+            string contentPath = this.WEBHOST.ContentRootPath;
 
-        string path = Path.Combine(this.WEBHOST.WebRootPath, "uploads");
-        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            string path = Path.Combine(this.WEBHOST.WebRootPath, "uploads");
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
 
-        string fileName = Path.GetFileName(file.FileName);
+            string fileName = Path.GetFileName(file.FileName);
 
-        using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
-        file.CopyTo(stream);
-        string uploadFile = fileName;
-        newDestination.Image = fileName;
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            file.CopyTo(stream);
+            string uploadFile = fileName;
+            newDestination.Image = fileName;
+        } 
+        else
+        {
+            newDestination.Image = "x"; // Set Default value
+        }
         newDestination.UserId = (int)id;
+
         DATABASE.Destinations.Add(newDestination);
         DATABASE.SaveChanges();
         
